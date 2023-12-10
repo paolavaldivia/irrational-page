0f503e3ca84d9942ba4add86c4553ccb2af4d0c6
Paola Valdivia <p.llerenavaldivia@criteo.com>
2023-12-10 14:40:37 +0100

    First draft

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,27 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Bebas Neue"', ...defaultTheme.fontFamily.sans],
+        "bebas-neue": ["Bebas Neue", "sans-serif"],
+      },
+      fontSize: {
+        ...defaultTheme.fontSize,
+        "8xl": "10rem",
+        "9xl": "12rem",
+      },
+      colors: {
+        "desert-sand": "#e0cda7",
+        "dusk-orange": "#eaac8b",
+        "twilight-lavender": "#957d95",
+        "canyon-red": "#cc8b85",
+        "sage-brush": "#b7ad99",
+      },
+    },
   },
   plugins: [],
-} satisfies Config;
+} as Config;
